f2f7c970671f661f66a4387117895630b58ebe31
JaacielBriseo <jaacielbris23@gmail.com>
2022-11-27 22:03:20 -0700

    Axios installed / Getting short url

--- src/UrlShorteningApp.jsx
+++ src/UrlShorteningApp.jsx
@@ -1,10 +1,19 @@
-import { HeroSection , Navbar} from "./components";
+import { useEffect } from 'react';
+import axios from 'axios';
+
+import { HeroSection, Navbar } from './components';
 
 export const UrlShorteningApp = () => {
+	useEffect(() => {
+		const resp = axios
+			.get('https://api.shrtco.de/v2/shorten?url=https://portfolio-jb-v1.netlify.app/home')
+			.then(({ data }) => console.log(data.result.short_link));
+	}, []);
+
 	return (
 		<>
-			<Navbar/>
-            <HeroSection/>
+			<Navbar />
+			<HeroSection />
 		</>
 	);
 };
